5e9587c253042411328395ebe649140159670e96
Esperanza Najera <esperanza.nave@gmail.com>
2023-06-22 10:12:32 -0700

    Starting project

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+</script>
+
+<template>
+<div>
+  <header>
+    <h1>Planificador de Gastos</h1>
+  </header>
+
+
+</div>
+</template>
+
+<style >
+:root{
+  --azul: #3b82f6;
+  --blanco: #fff;
+  --gris-claro: #F5F5F5;
+  --gris: #94a3b8;
+  --gris-oscuro: #64748b;
+  --negro: #000;
+}
+
+*,
+*:before,
+*:after{
+  box-sizing: inherit;
+}
+body{
+  font-size: 1.6rem;
+  font-family: "Lato", sans-serif;
+  background-color: var(--gris-claro);
+}
+h1{
+  font-size: 4rem;
+}
+h2{
+  font-size: 3rem;
+}
+header{
+  background-color: var(--azul)
+}
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--blanco);
+  text-align: center;
+}
+</style>
---
+++ src/components/Filtro.vue
@@ -0,0 +1,14 @@
+<script setup>
+  
+
+</script>
+
+<template>
+   <div>
+    <h2>Filtros</h2>
+   </div>
+</template>
+
+<style  scoped>
+
+</style>
